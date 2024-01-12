82b8f40012888cc76db39181ae13b8c7c112194a
JustusOmbok <ombokjustus@gmail.com>
2024-01-12 12:07:50 +0300

    type-annotated function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings, returns the result.
+
+    str1: The first string.
+    str2: The second string.
+    return: The concatenated string.
+    """
+    return str1 + str2
