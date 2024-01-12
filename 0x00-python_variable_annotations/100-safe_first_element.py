35c02eb863d386e136c65b45adc7e792620fe67a
JustusOmbok <ombokjustus@gmail.com>
2024-01-12 13:33:39 +0300

    type-annotated function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element of the input list, or None if the list is empty.
 
