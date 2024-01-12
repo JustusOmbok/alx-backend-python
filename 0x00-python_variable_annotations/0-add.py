0dd44024bc65a9dbe2f9a7ac559fa0d94caaf8e7
JustusOmbok <ombokjustus@gmail.com>
2024-01-12 11:53:03 +0300

    type-annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns their sum.
+
+    a: first float operand.
+    b: second float operand.
+    return: sum of the two operands.
+    """
+    return a + b
