c23583a4198ad354e1786144df752faf7f10aba8
JustusOmbok <ombokjustus@gmail.com>
2024-02-03 08:15:05 +0000

    unittests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from typing import Dict
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,6 +27,25 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/{}".format(org)
                 )
         self.assertEqual(result, resp)
+
+    def test_public_repos_url(self):
+        """Tests the `_public_repos_url` property."""
+        mock_payload = {
+                "repos_url": "https://api.github.com/orgs/example/repos"
+                }
+
+        with patch.object(
+                GithubOrgClient,
+                'org',
+                new_callable=PropertyMock,
+                return_value=mock_payload
+                ):
+            gh_org_client = GithubOrgClient("example")
+            expected_url = "https://api.github.com/orgs/example/repos"
+
+            result = gh_org_client._public_repos_url
+
+            self.assertEqual(result, expected_url)
 
 
 if __name__ == '__main__':
